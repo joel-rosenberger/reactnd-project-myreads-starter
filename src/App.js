0e3b450b3c494ab9472849c7df676d43321a6448
Joel Rosenberger <joelrosenberger@gmail.com>
2020-09-29 13:03:56 -0400

    Extract search into component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Bookshelf from './Bookshelf'
+import BookSearch from './BookSearch'
 
 import * as BooksAPI from './BooksAPI'
 import './App.css'
@@ -28,6 +29,10 @@ class BooksApp extends React.Component {
     }))
   }
 
+  closeSearch = () => {
+    this.setState({ showSearchPage: false });
+  }
+
   componentDidMount() {
     this.loadBooks();
   }
@@ -36,26 +41,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+            <BookSearch closeSearch = { this.closeSearch } />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
